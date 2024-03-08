a49aa4e82f963c4e9842e893e0485ee65e4a03a2
russ gazin <russgazin@gmail.com>
2024-03-08 17:38:19 -0600

    initial

---
+++ modules/db_subnet_group/variables.tf
@@ -0,0 +1,3 @@
+variable "name" {}
+variable "subnet_ids" { type = list(string) }
+variable "db_subnet_group_tag" { type = string }
---
+++ modules/tg/variables.tf
@@ -0,0 +1,6 @@
+variable "tg_name" {}
+variable "tg_port" {}
+variable "tg_protocol" {}
+variable "tg_tag" {}
+variable "tg_vpc_id" {}
+variable "instance_ids" { type = list(string) }
